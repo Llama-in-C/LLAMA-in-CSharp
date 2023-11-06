e0473619c43e5edd666f9d295e620ef83f7d0c80
Alexander Abushady <aabushady214@gmail.com>
2023-11-06 11:49:41 -0500

    Initial commit

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LLAMA_in_CSharp.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
