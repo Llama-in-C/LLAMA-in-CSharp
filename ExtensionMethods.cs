3610f620064d146fccaf8dd35e56a93770d80967
Alexander Abushady <aabushady214@gmail.com>
2023-11-07 09:11:08 -0500

    ExtensionMethods and Restructure
    Added ExtensionMethods file and restructured GGUF llama implementation into GGUF folder

--- ExtensionMethods.cs
+++ ExtensionMethods.cs
@@ -2,17 +2,17 @@
 
 public class ExtensionMethods
 {
-    static void replace_all(ref string s, ref string search, ref string replace) {
-        string result;
-        for (size_t pos = 0; ; pos += search.length()) {
-            auto new_pos = s.find(search, pos);
-            if (new_pos == std::string::npos) {
-                result += s.Substring(pos, s.size() - pos);
-                break;
-            }
-            result += s.Substring(pos, new_pos - pos) + replace;
-            pos = new_pos;
-        }
-        s = std::move(result);
-    }
+    // static void replace_all(ref string s, ref string search, ref string replace) {
+    //     string result;
+    //     for (Globals.sizeT pos = 0; ; pos += search.Length) {
+    //         auto new_pos = s.find(search, pos);
+    //         if (new_pos == std::string::npos) {
+    //             result += s.Substring(pos, s.size() - pos);
+    //             break;
+    //         }
+    //         result += s.Substring(pos, new_pos - pos) + replace;
+    //         pos = new_pos;
+    //     }
+    //     s = std::move(result);
+    // }
 }
