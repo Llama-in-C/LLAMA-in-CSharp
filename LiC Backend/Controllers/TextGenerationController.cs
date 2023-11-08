38cde112eefe116944fa27702b3564086aa28562
Alexander Abushady <aabushady214@gmail.com>
2023-11-07 22:29:13 -0500

    API checkpoint
    Basic API up

---
+++ LiC Backend/Controllers/TextGenerationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LiC_Backend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TextGenerationController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult GenerateText([FromBody] string inputText)
+    {
+        // Placeholder logic for text generation
+        var generatedText = $"Received: {inputText}";
+
+        return Ok(new { Input = inputText, Output = generatedText });
+    }
+}
