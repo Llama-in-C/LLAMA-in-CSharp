650cabb02d7989c4d59b9cf2a94b8ab537e31bac
Alexander Abushady <aabushady214@gmail.com>
2023-11-11 01:18:51 -0500

    Broken but checkpoint

--- LiC Backend/src/Python/lic.py
+++ LiC Backend/src/Python/lic.py
@@ -1,32 +1,27 @@
 import sys
 import os
 import time
-
 import win32file
 import win32pipe
-
 import traceback
 from multiprocessing import Process
+from pathlib import Path
+
 
 try:
     import flash_attn
 except ImportError:
-    print("Yo, exllamaV2 is WAYYYY better with flash_attn. I can be a pain to figure out, but it's worth it, trust!")
+    print('Yo, exllamaV2 is WAYYYY better with flash_attn. I can be a pain to figure out, but it\'s worth it, trust!')
 
-from exllamav2 import (
-    ExLlamaV2,
-    ExLlamaV2Config,
-    ExLlamaV2Cache,
-    ExLlamaV2Tokenizer,
+sys.path.insert(0, 'Z:\LLAMA in CSharp\LiC Backend\src\Python\External\exllamav2')
+
+from External.exllamav2.exllamav2 import (
+    model, cache, tokenizer, generator
 )
 
-from exllamav2.generator import (
-    ExLlamaV2BaseGenerator,
-    ExLlamaV2Sampler
+from External.exllamav2.exllamav2.generator import (
+    base, sampler
 )
-
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 def pipe_server():
@@ -47,7 +42,6 @@ def pipe_server():
             print("Waiting for client")
             win32pipe.ConnectNamedPipe(pipe, None)
             print("Got client")
-            output = ""
             while True:
                 # Read the message from the named pipe
                 result, data = win32file.ReadFile(pipe, 64 * 1024)
@@ -64,7 +58,8 @@ def pipe_server():
                 time_total = time_end - time_begin
 
                 print(f"Generated text:\n{output}\n")
-                print(f"Response generated in {time_total:.2f} seconds, {max_new_tokens} tokens, {max_new_tokens / time_total:.2f} tokens/second")
+                print(f"Response generated in {time_total:.2f} seconds, {max_new_tokens} tokens, "
+                      f"{max_new_tokens / time_total:.2f} tokens/second")
 
                 # Write the result back to the named pipe
                 win32file.WriteFile(pipe, str.encode(output))
@@ -83,26 +78,34 @@ if __name__ == '__main__':
     server_process.start()
     server_process.join()
 
+config = model.ExLlamaV2Config()
+# Get the current directory (where your script is)
+current_dir = os.path.dirname(os.path.realpath(__file__))
 
-config = ExLlamaV2Config()
-#config.model_dir = model_directory
+# The path to the adjacent directory
+adjacent_dir = os.path.join(current_dir, '../Models')
+
+# The path to the file in the adjacent directory
+model_directory = os.path.join(adjacent_dir, 'Dawn-v2-70B-2.55bpw-h6-exl2')
+
+config.model_dir = model_directory
 config.prepare()
 
-model = ExLlamaV2(config)
-#print("Loading model: " + model_directory)
+model = model.ExLlamaV2(config)
+print("Loading model: " + model_directory)
 
-cache = ExLlamaV2Cache(model, lazy=True)
+cache = cache.ExLlamaV2Cache(model, lazy=True)
 model.load_autosplit(cache)
 
-tokenizer = ExLlamaV2Tokenizer(config)
+tokenizer = tokenizer.ExLlamaV2Tokenizer(config)
 
 # Initialize generator
 
-generator = ExLlamaV2BaseGenerator(model, cache, tokenizer)
+generator = base.ExLlamaV2BaseGenerator(model, cache, tokenizer)
 
 # Generate some text
 
-settings = ExLlamaV2Sampler.Settings()
+settings = sampler.ExLlamaV2Sampler.Settings()
 settings.temperature = 1.25
 settings.top_k = 0
 settings.top_p = 0.8
